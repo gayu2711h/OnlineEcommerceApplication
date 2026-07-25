bb0f1d763047c921fa4938f581085c59b8a9ddb6
Pranjali_Thorat <tpranjali24@gmail.com>
2026-07-25 11:09:38 +0530

    Product Added API

--- ecom_backend/src/main/java/com/ecom/dtos/ProductRequestDto.java
+++ ecom_backend/src/main/java/com/ecom/dtos/ProductRequestDto.java
@@ -1,4 +1,4 @@
-package com.ecom.dto;
+package com.ecom.dtos;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
--- ecom_backend/src/main/java/com/ecom/exception_handler/APIException.java
+++
@@ -1,9 +0,0 @@
-package com.ecom.exception_handler;
-
-public class APIException extends RuntimeException 
-{
-	public APIException(String msg)
-	{
-		super(msg);
-	}
-}
